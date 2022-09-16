e1ee46298567788c03a89b9d8e4f07e23763fd18
Egemen MEDE <egemenmede@gmail.com>
2022-09-17 00:10:10 +0300

    Update extensions.dart

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,21 +1,148 @@
+import 'package:flutter/foundation.dart';
+import 'package:iban_utilities/country_enums.dart';
+
 extension StringExtensions on String {
-  String countryCode() {
-    return replaceAll(' ', '').substring(0, 2);
+  String countryCode(Country country) {
+    switch (country) {
+      case Country.turkish:
+        {
+          return replaceAll(' ', '').substring(0, 2);
+        }
+
+      case Country.england:
+        {
+          return replaceAll(' ', '').substring(0, 2);
+        }
+
+      case Country.germany:
+        {
+          return replaceAll(' ', '').substring(0, 2);
+        }
+
+      default:
+        {
+          return replaceAll(' ', '').substring(0, 2);
+        }
+    }
   }
 
-  String checkDigits() {
-    return replaceAll(' ', '').substring(2, 4);
+  String checkDigits(Country country) {
+    switch (country) {
+      case Country.turkish:
+        {
+          return replaceAll(' ', '').substring(2, 4);
+        }
+
+      case Country.england:
+        {
+          return replaceAll(' ', '').substring(2, 4);
+        }
+
+      case Country.germany:
+        {
+          return replaceAll(' ', '').substring(2, 4);
+        }
+
+      default:
+        {
+          return replaceAll(' ', '').substring(2, 4);
+        }
+    }
   }
 
-  String bankCode() {
-    return replaceAll(' ', '').substring(4, 9);
+  String bankCode(Country country) {
+    switch (country) {
+      case Country.turkish:
+        {
+          return replaceAll(' ', '').substring(4, 9);
+        }
+
+      case Country.england:
+        {
+          return replaceAll(' ', '').substring(4, 8);
+        }
+
+      case Country.germany:
+        {
+          return replaceAll(' ', '').substring(4, 12);
+        }
+
+      default:
+        {
+          return replaceAll(' ', '').substring(4, 9);
+        }
+    }
   }
 
-  String nationalCheckDigit() {
-    return replaceAll(' ', '').substring(9, 10);
+  String sortCode(Country country) {
+    switch (country) {
+      case Country.turkish:
+        {
+          return replaceAll(' ', '').substring(0, 0);
+        }
+
+      case Country.england:
+        {
+          return replaceAll(' ', '').substring(8, 14);
+        }
+
+      case Country.germany:
+        {
+          return replaceAll(' ', '').substring(0, 0);
+        }
+
+      default:
+        {
+          return replaceAll(' ', '').substring(0, 0);
+        }
+    }
   }
 
-  String bankAccountNumber() {
-    return replaceAll(' ', '').substring(10, 26);
+  String nationalCheckDigit(Country country) {
+    switch (country) {
+      case Country.turkish:
+        {
+          return replaceAll(' ', '').substring(9, 10);
+        }
+
+      case Country.england:
+        {
+          return replaceAll(' ', '').substring(0, 0);
+        }
+
+      case Country.germany:
+        {
+          return replaceAll(' ', '').substring(0, 0);
+        }
+
+      default:
+        {
+          return replaceAll(' ', '').substring(9, 10);
+        }
+    }
+  }
+
+  String bankAccountNumber(Country country) {
+    switch (country) {
+      case Country.turkish:
+        {
+          return replaceAll(' ', '').substring(10, 26);
+        }
+
+      case Country.england:
+        {
+          return replaceAll(' ', '').substring(14, 22);
+        }
+
+      case Country.germany:
+        {
+          return replaceAll(' ', '').substring(12, 22);
+        }
+
+      default:
+        {
+          return replaceAll(' ', '').substring(10, 26);
+        }
+    }
   }
 }
