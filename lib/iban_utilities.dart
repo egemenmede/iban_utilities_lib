3e933763db0d235ef9b1c81493b114b5c1dc6903
Egemen MEDE <egemenmede@gmail.com>
2022-09-17 00:38:09 +0300

    Update iban_utilities.dart

--- lib/iban_utilities.dart
+++ lib/iban_utilities.dart
@@ -10,6 +10,7 @@ class IbanUtilities {
   String countryCode(Country country) => iban.countryCode(country);
   String checkDigits(Country country) => iban.checkDigits(country);
   String bankCode(Country country) => iban.bankCode(country);
+  String branchCode(Country country) => iban.branchCode(country);
   String sortCode(Country country) => iban.sortCode(country);
   String nationalCheckDigit(Country country) => iban.nationalCheckDigit(country);
   String bankAccountNumber(Country country) => iban.bankAccountNumber(country);
