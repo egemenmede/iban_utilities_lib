935768a0612e875024e1f5cc33358fee81644a3d
Egemen MEDE <egemenmede@gmail.com>
2022-09-17 16:28:33 +0300

    Update iban_utilities.dart

--- lib/iban_utilities.dart
+++ lib/iban_utilities.dart
@@ -1,6 +1,6 @@
 library iban_utilities;
 
-import 'package:iban_utilities/country_enums.dart';
+import 'package:iban_utilities/enums_countries.dart';
 import 'package:iban_utilities/extensions.dart';
 
 class IbanUtilities {
@@ -8,11 +8,13 @@ class IbanUtilities {
   IbanUtilities(this.iban);
 
   bool ibanValidate(Country country) => iban.ibanValidate(country, iban);
-  String countryCode(Country country) => iban.countryCode(country);
-  String checkDigits(Country country) => iban.checkDigits(country);
-  String bankCode(Country country) => iban.bankCode(country);
-  String branchCode(Country country) => iban.branchCode(country);
-  String sortCode(Country country) => iban.sortCode(country);
-  String nationalCheckDigit(Country country) => iban.nationalCheckDigit(country);
-  String bankAccountNumber(Country country) => iban.bankAccountNumber(country);
+  String countryCode(Country country) => iban.getCountryCode(country);
+  String checkDigits(Country country) => iban.getCheckDigits(country);
+  String bankCode(Country country) => iban.getBankCode(country);
+  String branchCode(Country country) => iban.getBranchCode(country);
+  String sortCode(Country country) => iban.getSortCode(country);
+  String nationalCheckDigit(Country country) => iban.getNationalCheckDigit(country);
+  String bankAccountNumber(Country country) => iban.getBankAccountNumber(country);
+
+  //TODO String generateIban(Country country) => iban.generateIban(country);
 }
