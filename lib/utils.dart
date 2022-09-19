60b9bed0299c245c1ba2b4f18f4d42079b18b697
Egemen MEDE <egemenmede@gmail.com>
2022-09-19 09:10:06 +0300

    Update utils.dart

--- lib/utils.dart
+++ lib/utils.dart
@@ -5,10 +5,12 @@ import 'package:iban_utilities/enums_countries.dart';
 class Utils {
   static final Random _random = Random.secure();
 
+  /// Verilen iki rakam arasında random sayı döndürür.
   static int generateCheckDigits(int min, int max) {
     return min + _random.nextInt(max - min);
   }
 
+  /// A-Z arasındaki büyük harflerden istenilen uzunlukta random bir metin döndürür.
   static String generateRandomUpperCaseAlpha(int length) {
     const availableChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     final randomString = List.generate(length,
@@ -18,6 +20,7 @@ class Utils {
     return randomString;
   }
 
+  /// 0-9 arasındaki rakamlardan istenilen uzunlukta random bir metin döndürür.
   static String generateRandomNumeric(int length) {
     const availableChars = '1234567890';
     final randomString = List.generate(length,
@@ -27,6 +30,7 @@ class Utils {
     return randomString;
   }
 
+  /// Verilen bilgilere göre Iban Kontrol Numarasını geri döndürür.
   static String _getIbanChecksum(String str){
     List<String> strArr =  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     List<String> intArr =  ["10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35"];
@@ -42,6 +46,7 @@ class Utils {
     return (98-remainder).toString();
   }
 
+  /// Bir Iban numarasını oluşturarak geri döndürür.
   static String generateIban(Country country) {
     String staticSampleBankCode = "00062";
     switch (country) {
@@ -50,7 +55,7 @@ class Utils {
           String lastPart = Utils.generateRandomNumeric(16);
           String template =  _getIbanChecksum("TR00${staticSampleBankCode}0$lastPart");
           var newIban = "TR$template${staticSampleBankCode}0$lastPart";
-          print("newIban: $newIban");
+          // print("newIban: $newIban");
           return newIban;
         }
 
@@ -60,5 +65,4 @@ class Utils {
         }
     }
   }
-
 }
