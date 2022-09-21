f86899f990f374854dc65533cafa9d632b45d54c
Egemen MEDE <egemenmede@gmail.com>
2022-09-21 07:54:01 +0300

    Update utils.dart

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:iban_utilities_lib/bank.dart';
 import 'package:iban_utilities_lib/enums_countries.dart';
 
 class Utils {
@@ -31,35 +32,170 @@ class Utils {
   }
 
   /// Verilen bilgilere göre Iban Kontrol Numarasını geri döndürür.
-  static String _getIbanChecksum(String str){
-    List<String> strArr =  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    List<String> intArr =  ["10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35"];
+  static String _getIbanChecksum(String str) {
+    List<String> strArr = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z"
+    ];
+    List<String> intArr = [
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+      "24",
+      "25",
+      "26",
+      "27",
+      "28",
+      "29",
+      "30",
+      "31",
+      "32",
+      "33",
+      "34",
+      "35"
+    ];
 
     var iban = str.substring(4) + str.substring(0, 4);
 
-    for (int i = 0; i < strArr.length; i++){
+    for (int i = 0; i < strArr.length; i++) {
       iban = iban.replaceAll(strArr[i], intArr[i]);
     }
 
     var ibanInt = BigInt.parse(iban);
     var remainder = ibanInt.remainder(BigInt.from(97)).toInt();
     var temp = "";
-    ((98-remainder).toString().length == 1) ? temp = "0${98-remainder}" : temp = (98-remainder).toString();
+    ((98 - remainder).toString().length == 1)
+        ? temp = "0${98 - remainder}"
+        : temp = (98 - remainder).toString();
     return temp;
+  }
+
+  static String generateBankCode(Country country) {
+    switch (country) {
+      case Country.turkish:
+        {
+          List<Bank> bankListTR = getBankObjectList(country);
+
+          final Random random = Random.secure();
+          var bank = bankListTR[random.nextInt(bankListTR.length)];
+
+          return bank.bankCode;
+        }
+
+      default:
+        {
+          return "00061";
+        }
+    }
+  }
+
+  static Bank getBankObject(Country country, String activeBankCode) {
+    List<Bank> bankObjectList = getBankObjectList(country).where((element) => element.bankCode == activeBankCode).toList();
+    Bank bankObj;
+    if (bankObjectList.isNotEmpty) {
+      bankObj = bankObjectList[0];
+    }else{
+      bankObj = Bank("","","");
+    }
+
+    return bankObj;
+  }
+
+  static List<Bank> getBankObjectList(Country country) {
+    switch (country) {
+      case Country.turkish:
+        {
+          List<Bank> bankListTR = [
+            Bank("00209", "Ziraat Katılım Bankası", "ziraat-katilim-bankasi"),
+            Bank("00206", "Türkiye Finans", "turkiye-finans"),
+            Bank("00205", "Kuveyt Türk", "kuveyt-turk"),
+            Bank("00203", "Albaraka Türk", "albaraka-turk"),
+            Bank("00146", "Odea Bank", "odea-bank"),
+            Bank("00143", "Aktif Bank", "aktif-yatirim-bankasi"),
+            Bank("00142", "BankPozitif", "bankpozitif"),
+            Bank("00135", "Anadolu Bank", "anadolu-bank"),
+            Bank("00134", "Denizbank", "denizbank"),
+            Bank("00125", "Burgan Bank", "burgan-bank"),
+            Bank("00124", "ABank", "alternatif-bank"),
+            Bank("00123", "HSBC", "hsbc"),
+            Bank("00111", "Finansbank", "finansbank"),
+            Bank("00109", "Tekstilbank", "tekstil-bankasi"),
+            Bank("00108", "T-Bank", "turkland-bank"),
+            Bank("00103", "Fibabanka", "fibabanka"),
+            Bank("00100", "Adabank", "adabank"),
+            Bank("00099", "ING Bank", "ing-bank"),
+            Bank("00096", "Turkish Bank", "turkish-bank"),
+            Bank("00092", "Citibank", "citibank"),
+            Bank("00091", "A&T Bank", "arap-turk-bankasi"),
+            Bank("00071", "Fortis", "fortis"),
+            Bank("00067", "Yapı ve Kredi Bankası", "yapi-ve-kredi-bankasi"),
+            Bank("00064", "İş Bankası", "is-bankasi"),
+            Bank("00062", "Garanti Bankası", "garanti-bankasi"),
+            Bank("00059", "Şekerbank", "sekerbank"),
+            Bank("00046", "Akbank", "akbank"),
+            Bank("00032", "Türk Ekonomi Bankası (TEB)", "turk-ekonomi-bankasi"),
+            Bank("00015", "VakıfBank", "vakiflar-bankasi"),
+            Bank("00012", "Halkbank", "halkbank"),
+            Bank("00010", "Ziraat Bankası", "ziraat-bankasi"),
+          ];
+
+          return bankListTR;
+        }
+
+      default:
+        {
+          return [];
+        }
+    }
   }
 
   /// Bir Iban numarasını oluşturarak geri döndürür.
   static String generateIban(Country country) {
-    String staticSampleBankCode = "00062";
+    String bankCode = generateBankCode(country);
     switch (country) {
       case Country.turkish:
         {
           String lastPart = Utils.generateRandomNumeric(16);
           print("----------------------------------");
           print("lastPart(16): ${lastPart.length}");
-          String template =  _getIbanChecksum("TR00${staticSampleBankCode}0$lastPart");
+          String template = _getIbanChecksum("TR00${bankCode}0$lastPart");
           print("template(2): $template");
-          var newIban = "TR$template${staticSampleBankCode}0$lastPart";
+          var newIban = "TR$template${bankCode}0$lastPart";
           print("newIban: $newIban");
           print("----------------------------------");
           return newIban;
